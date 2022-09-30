ab893fee25d842437a5c20a4a045427b96e1916a
hapiz2022 <112926419+hapiz2022@users.noreply.github.com>
2022-10-01 03:14:00 +0700

    Praktikum 3

---
+++ Luas Lingkaran dan Volume Tabung.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   float r, t, lT, vT, lL;
+   float phi = 3.14;
+
+   cout << "Menghitung Luas Lingkaran & Volume Tabung" << endl;
+   cout << endl;
+   cout << "Masukan Jari-jari : ";
+   cin  >> r;
+   cout << "Masukan Tinggi    : ";
+   cin >> t;
+
+   lT = (2 * phi * r * (r + t));
+   vT = (phi * t * (r * r));
+   lL = phi * r * r;
+
+   cout << endl;
+   cout << "Luas Lingkaran   : " << lL << endl;
+   cout << "Luas Tabung	    : " << lT << endl;
+   cout << "Volume Tabung	  : " << vT << endl;
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# 1. perbedaan dari penempatan notasi didepan variable (A--/A++) dengan penempatan notasi dibelakang variable (--A/++A)!
+
+* Pre-increment ++a Tambah a sebanyak 1 angka, lalu tampilkan hasilnya
+* Post-increment a++ Tampilkan nilai a, lalu tambah a sebanyak 1 angka
+* Pre-decrement --a Kurangi a sebanyak 1 angka, lalu tampilkan hasilnya
+* Post-decrement a-- Tampilkan nilai a, lalu kurangi a sebanyak 1 angka
+
+# 2. Penjelasan Kode Dibawah ini :
+![Penjelasan Kode](https://github.com/hapiz2022/C-Task3/blob/main/Penjelasan%20kode.jpg)
